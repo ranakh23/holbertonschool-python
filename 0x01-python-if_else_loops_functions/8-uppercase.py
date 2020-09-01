3b179f869dc490aa82bd3070c76fe58f21b689b9
vagrant <vagrant@vagrant.vm>
2020-09-01 20:19:01 +0000

    dsfjh

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    new = ""
     for i in range(len(str)):
         if ord(str[i]) in range(97, 123):
-            str[i] = chr(ord(str[i]) - 32)
-    print("{}".format(str))
+            new += chr(ord(str[i]) - 32)
+        else:
+            new += str[i]
+    print("{}".format(new))
