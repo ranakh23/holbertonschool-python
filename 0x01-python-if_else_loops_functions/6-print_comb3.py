d36fadc54e848513d163203f9d34c34037c7f856
vagrant <vagrant@vagrant.vm>
2020-09-01 19:49:47 +0000

    dfjh

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(i, 10):
+        if i != j and i != 8:
+            print("{}{}".format(i, j), end = ', ')
+        elif i != j and i == 8:
+            print("{}{}".format(i, j))
