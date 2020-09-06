c3f77296b578b517504b0061ffe97af1c8a25c21
vagrant <vagrant@vagrant.vm>
2020-09-06 18:22:08 +0000

    dfjh

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments".format(n))
     for i in range(1, len(sys.argv)):
-        print("{} : {}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
