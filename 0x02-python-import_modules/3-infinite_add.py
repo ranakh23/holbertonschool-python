8748b1a5cfc20cf8a9b0febdd5a274a139fed0ee
vagrant <vagrant@vagrant.vm>
2020-09-06 18:43:26 +0000

    dfjh

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 from sys import argv
 if __name__ == "__main__":
     i = 0
-    for j in range(len(argv) - 1):
+    for j in range(1, len(argv)):
         i += int(argv[j])
     print("{}".format(i))
