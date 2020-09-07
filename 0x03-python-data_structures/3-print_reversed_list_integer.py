10bfcc26d334bcb9711f903547bb896cd53ea896
vagrant <vagrant@vagrant.vm>
2020-09-07 21:07:35 +0000

    sdjfh

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range(1, len(my_list) + 1):
-        print("{}".format(my_list[-i]))
+    for index in my_list[::-1]:
+        print("{:d}".format(index))
