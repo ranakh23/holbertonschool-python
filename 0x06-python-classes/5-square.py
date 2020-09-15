12f208efefbff4cdf7ed5e24d126db6c17a1696b
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2020-09-15 21:40:13 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
+"""
+dsf
+"""
 class Square:
+    """
+    sdf
+    """
     def __init__(self, size=0):
         self.__size = size
 
     # Property
     @property
     def size(self):
+        """
+        dsfs
+        """
         return self.__size
 
     # Setter modifies
     @size.setter
     def size(self, value):
+        """
+        dsfsd
+        """
         if type(value) != int:
             raise TypeError('size must be an integer')
         elif value < 0:
@@ -19,9 +31,15 @@ class Square:
             self.__size = value
 
     def area(self):
+        """
+        dsfds
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """
+        dsfs
+        """
         size = self.__size
 
         if size == 0:
