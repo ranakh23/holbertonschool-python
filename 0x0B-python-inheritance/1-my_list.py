f31704186dbf628a6edf27f238efc2c0a1d8306a
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2020-09-30 19:01:21 +0300

    Update 1-my_list.py

--- 0x0B-python-inheritance/1-my_list.py
+++ 0x0B-python-inheritance/1-my_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""MyList
-"""
+# 1-my_list.py
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """Contains list
-    """
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """Prints self in sorted format
-        """
-
+        """Print a list in sorted ascending order."""
         print(sorted(self))
