891667b23b71f195a150b1c31d65f9d4d348efc6
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2020-09-30 18:57:19 +0300

    Update 5-base_geometry.py

--- 0x0B-python-inheritance/5-base_geometry.py
+++ 0x0B-python-inheritance/5-base_geometry.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""BaseGeometry
-"""
+# 5-base_geometry.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines an empty class BaseGeometry."""
 
 
 class BaseGeometry:
+    """Represent base geometry."""
     pass
