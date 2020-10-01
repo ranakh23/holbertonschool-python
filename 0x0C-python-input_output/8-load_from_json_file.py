180febcce855b796866049e8e389bbd22eafec7b
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2020-10-01 20:27:54 +0300

    Update 8-load_from_json_file.py

--- 0x0C-python-input_output/8-load_from_json_file.py
+++ 0x0C-python-input_output/8-load_from_json_file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""load_from_json_file
 """
+Contains the "load_from_json_file" function
+"""
+
 import json
 
 
 def load_from_json_file(filename):
-    """Returns created object from a JSON file
-    """
-
-    with open(filename, mode="r", encoding="UTF-8") as readFile:
-       return json.load(readFile)
+    """creates an Object from a "JSON file" """
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
