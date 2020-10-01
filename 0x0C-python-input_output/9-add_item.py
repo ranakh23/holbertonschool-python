6d3709620822665f0cf4e694dc6e5250b885fe9b
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2020-10-01 20:40:54 +0300

    Update 9-add_item.py

--- 0x0C-python-input_output/9-add_item.py
+++ 0x0C-python-input_output/9-add_item.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-
-from sys import argv
+"""Module for saving to json"""
 import json
+import os.path
+import sys
+from sys import argv
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-'''
-Write a script that adds all
-arguments to a Python list, and then
-save them to a file
-'''
+filename = "add_item.json"
+json_list = []
 
+if os.path.exists(filename):
+    json_list = load_from_json_file(filename)
 
-argc = len(argv)
+for index in argv[1:]:
+    json_list.append(index)
 
-filename = 'add_item.json'
-my_list = []
-try:
-    my_list = load_from_json_file(filename)
-except BaseException:
-    pass
-for items in range(1, argc):
-    my_list.append(argv[items])
-save_to_json_file(my_list, filename)
+save_to_json_file(json_list, filename)
