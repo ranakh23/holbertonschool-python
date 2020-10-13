2be4a33960b8cd88382d13095c2fddba2ce972ba
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2020-10-13 21:00:15 +0300

    Update 0-minoperations.py

--- 0x0E-minimum_operations/0-minoperations.py
+++ 0x0E-minimum_operations/0-minoperations.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-'''
-method that calculates the fewest number of operations
-needed to result in exactly n H characters in the file.
-'''
+import math
 
 
 def minOperations(n):
-    result = 0
-    number = n
-    for i in range(2, number + 1):
-        while (number % i == 0):
-            result += i
-            number /= i
-            if number < i:
-                break
-    return result
+    """ Calculates the fewest number of operations needed to result in
+        exactly n H characters.
+        Args:
+            @n: integer
+        Return:
+            the fewest number of operations needed to result in n H characters
+    """
+    suma = 0
+    if n <= 1:
+        return suma
+    for i in range(2, int(math.sqrt(n) + 1)):
+        while n % i == 0:
+            suma += i
+            n = n // i
+    if n > 1:
+        suma += n
+    return suma
