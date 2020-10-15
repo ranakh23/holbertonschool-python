7139f9ba78ec6fceac38abf6e6482424c77ff61e
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2020-10-15 23:48:36 +0300

    Update 0-body_size.sh

--- 0x0F-python-network_0/0-body_size.sh
+++ 0x0F-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#takes in a URL, sends a request to that URL, and displays the size of the bodyof the response
-curl -s $1 --include | grep -i Content-Length | awk '{print $2}'
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
