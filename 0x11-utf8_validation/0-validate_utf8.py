f7574a45b2bcc51852226d315727927fdb53d8f4
ranakh23 <42887031+ranakh23@users.noreply.github.com>
2020-10-20 23:19:36 +0300

    Update 0-validate_utf8.py

--- 0x11-utf8_validation/0-validate_utf8.py
+++ 0x11-utf8_validation/0-validate_utf8.py
@@ -1,48 +1,31 @@
 #!/usr/bin/python3
 """
-0x11. UTF-8 Validation
+method that determines if a given data set represents a valid UTF-8
 """
-
-
-from itertools import takewhile
-
-
-def to_bits(bytes):
-    """
-    Convert bytes to bits
-    """
-    for byte in bytes:
-        num = []
-        exp = 1 << 8
-        while exp:
-            exp >>= 1
-            num.append(bool(byte & exp))
-            yield num
 
 
 def validUTF8(data):
     """
-    Method that determines if a given data set
-    represents a valid UTF-8 encoding.
+    Determines if a data set represents a valid UTF-8
+    UTF could be until 4 bytes, managing them as UTF encode
+    Return: True if is a valid UTF-8 encoding or False
     """
-    bits = to_bits(data)
-    for byte in bits:
-        # single byte char
-        if byte[0] == 0:
-            continue
+    n_bytes = 0
 
-        # multi-byte character
-        amount = sum(takewhile(bool, byte))
-        if amount <= 1:
-            return False
-        if amount >= 4:
-            return False
+    for n in data:
+        byte = format(n, '#010b')[-8:]
+        if n_bytes == 0:
+            if byte[0] == '1':
+                n_bytes = len(byte.split('0')[0])
+            if n_bytes > 4 or n_bytes == 1:
+                return False
+            if n_bytes == 0:
+                continue
+        else:
+            if not byte.startswith('10'):
+                return False
+        n_bytes -= 1
 
-        for _ in range(amount - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
-                return False
-            if byte[0:2] != [1, 0]:
-                return False
+    if n_bytes != 0:
+        return False
     return True
